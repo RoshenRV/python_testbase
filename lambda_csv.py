ec469c2475d56e1a5916054e9183caa218606baf
EC2 Default User <ec2-user@ip-172-31-10-3.eu-west-1.compute.internal>
2018-03-28 10:39:50 +0000

    Added checged

--- lambda_csv.py
+++ lambda_csv.py
@@ -2,6 +2,8 @@ import csv
 
 with open('sampleData.csv', 'r') as csv_file: #this line opens up the file using a context manager 'r' denote read
 	csv_reader = csv.reader(csv_file) #This is a reader method usies ";" as a delimiter by default
-
+	
+	next(csv_reader)
+	
 	for line in csv_reader:
-		print(line)
+		print(line[4])
