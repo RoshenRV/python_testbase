18a47aebb71359c2169260b95c1910ce4c16f753
Roshen <roshenreji2007@gmail.com>
2018-03-29 16:09:40 +0000

    V3]

--- lambda_csv.py
+++ lambda_csv.py
@@ -12,14 +12,15 @@ def csv_dict_reader(csvFile_obj):
 
 def csv_reader(csvFile_obj): #This function is to hold the fieldname for writing it back to CSV file
 	csv_reader_data = csv.reader(csvFile_obj)
-	header = csv_reader_data.next() #Get the header of the CSV for fieldname
+	header = csv_reader_data.next(); #Get the header of the CSV for fieldname
 	return header
 
 #MAIN
 #-----------------------------------------------------------------------------------
 with open('sampleData.csv', 'r') as csv_file: #this line opens up the file using a context manager 'r' denote read
 	csv_dict_reader(csv_file)
-	csv_reader(csv_file)
+	header = csv_reader(csv_file)
+	print header
 	
 	#next(csv_reader)
 	
