a1f61215fdb8f88d77175d9652857ddd43265fdf
Roshen <roshenreji2007@gmail.com>
2018-03-30 09:39:48 +0000

    Working Version #Almost

--- lambda_csv.py
+++ lambda_csv.py
@@ -1,35 +1,29 @@
 import csv
 
-#FUNCTIONS
-#-----------------------------------------------------------------------------------
-
-def csv_dict_reader(csvFile_obj):
-	csv_dictReader_data = csv.DictReader(csvFile_obj)
-	
-	for line in csv_dictReader_data:
-		sum =  int(line['Nonproductive time']) - 15
-		print(sum)
-
-def csv_reader(csvFile_obj): #This function is to hold the fieldname for writing it back to CSV file
-	csv_reader_data = csv.reader(csvFile_obj)
-	header = csv_reader_data.next(); #Get the header of the CSV for fieldname
-	return header
-
-#MAIN
-#-----------------------------------------------------------------------------------
-with open('sampleData.csv', 'r') as csv_file: #this line opens up the file using a context manager 'r' denote read
-	csv_dict_reader(csv_file)
-	header = csv_reader(csv_file)
-	print header
-	
-	#next(csv_reader)
-	
-		
-		
-		
-#	with open('new_file.csv', 'w') as new_file:
-#		fieldnames = i
-#		csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
-	#
-	#	for line in csv_reader:
-	#		csv_writer.writerow(line[4])
+in_file = open("sampleData.csv", "rb")
+csv_reader_data = csv.reader(in_file)
+header = next(csv_reader_data ) #Get the header of the CSV for fieldname
+out_file = open("new_file.csv", "wb")
+writer = csv.writer(out_file)
+#print header
+headerIndex = []
+for line in header:
+    if 'time' in line:
+        print line, 'Index - ', header.index(line)
+        headerIndex = headerIndex + [header.index(line)]
+        print headerIndex
+    else: 
+        print 'fail'
+for line2 in csv_reader_data:
+    hms = ''
+    for i in headerIndex:
+        print line2[i], 'Index - ', i
+        if line2[i] == '':
+            line2[i] = 0
+        seconds = int(line2[i])
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        hms = "%d:%02d:%02d" % (h, m, s)
+        line2[i] = hms
+        print hms
+    writer.writerow(line2)
