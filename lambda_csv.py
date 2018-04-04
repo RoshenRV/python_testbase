e3411c2cbe36a2701990311c9a3539619a6c743e
Roshen <roshenreji2007@gmail.com>
2018-04-04 11:43:42 +0000

    Fixed Working Verison

--- lambda_csv.py
+++ lambda_csv.py
@@ -1,29 +1,50 @@
+import boto3
 import csv
 
-in_file = open("sampleData.csv", "rb")
-csv_reader_data = csv.reader(in_file)
-header = next(csv_reader_data ) #Get the header of the CSV for fieldname
-out_file = open("new_file.csv", "wb")
-writer = csv.writer(out_file)
-#print header
-headerIndex = []
-for line in header:
-    if 'time' in line:
-        print line, 'Index - ', header.index(line)
-        headerIndex = headerIndex + [header.index(line)]
-        print headerIndex
-    else: 
-        print 'fail'
-for line2 in csv_reader_data:
-    hms = ''
-    for i in headerIndex:
-        print line2[i], 'Index - ', i
-        if line2[i] == '':
-            line2[i] = 0
-        seconds = int(line2[i])
-        m, s = divmod(seconds, 60)
-        h, m = divmod(m, 60)
-        hms = "%d:%02d:%02d" % (h, m, s)
-        line2[i] = hms
-        print hms
-    writer.writerow(line2)
+s3 = boto3.resource('s3')
+
+def csv_converter(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    original_temp_filepath = '/tmp/' + key
+    modified_temp_filepath = '/tmp/modified-'+ key
+    modified_filename = 'modified-'+key
+    
+    s3.meta.client.download_file(bucket, key, original_temp_filepath)
+    
+    original_file = open(original_temp_filepath, "rb")
+    csv_reader_data = csv.reader(original_file)
+    
+    header = next(csv_reader_data)
+    
+    modified_file = open(modified_temp_filepath, "wb")
+    csv_writer_data = csv.writer(modified_file)
+    
+    headerIndex = []
+    
+    for line in header:
+        if 'time' in line:
+            headerIndex = headerIndex + [header.index(line)]
+    csv_writer_data.writerow(header)
+    
+    for line2 in csv_reader_data:
+        hms = ''
+        for x in headerIndex:
+            if line2[x] == '':
+                line2[x] = 0
+            seconds = int(line2[x])
+            m, s = divmod(seconds, 60)
+            h, m = divmod(m, 60)
+            hms = "%d:%02d:%02d" % (h, m, s)
+            line2[x] = hms
+        csv_writer_data.writerow(line2)
+        modified_file.flush()
+        #print(line2)
+    
+    temp_file = open(modified_temp_filepath, "rb")
+    csv_reader_data2 = csv.reader(temp_file)
+    
+    for line3 in csv_reader_data2:
+        print(line3)
+        
+    #s3.meta.client.upload_file(modified_temp_filepath, bucket, modified_filename)
